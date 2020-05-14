0688173cc5852b36684ebcdd5063da2ab650f267
zspace09 <352045777@qq.com>
2020-05-14 17:18:55 +0800

    自动添加标签(add, sub, mul, div四种)

--- testing/conftest.py
+++ testing/conftest.py
@@ -1,2 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import pytest
+import yaml
+
+steps = yaml.safe_load(open("../config/steps.yml"))
+
+
+def pytest_collection_modifyitems(session, config, items:list):
+    # print(items)
+    new_items = []
+    for step in steps:
+        for item in items:
+            if step in item.nodeid:
+                item.add_marker(step)
+                new_items.append(item)
+    items[:] = new_items
+
+
